1842089f0ff6e6af486b5572aebc3a2de321a47e
Danial Radkianpour <150389319+BeingBornn@users.noreply.github.com>
2024-04-13 05:56:17 +0200

    Time: 11 ms (38.54%), Space: 6 MB (29.68%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,44 @@
+// function to get the number of the digit
+int get_digit(int  n)
+{
+    unsigned int    i;
+
+    i = 1;
+    while(n != 0)
+    {
+        n /= 10;
+        if(n != 0)
+            i++;
+    }
+    return (i);
+}
+
+bool isPalindrome(int x)
+{
+    if(x < 0)
+        return (false);
+    if(x < 10)
+        return (true);
+    int i;
+    int j;
+    int digit;
+
+    digit = get_digit(x);
+    char str[digit + 1];
+    sprintf(str, "%d", x);
+    i = 0;
+    j = digit - 1;
+
+    if(x > 9)
+    {
+        while(i < digit / 2)
+        {
+            if(str[i] != str[j])
+                return(false);
+            i++;
+            j--;  
+        }
+    }
+    return (true);
+}
+  
